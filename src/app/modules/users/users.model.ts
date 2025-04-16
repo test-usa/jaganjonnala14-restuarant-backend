2af53201ef1888a0a8323bfb4d8bff0509e8c561
safi miazi <mohibullamiazi@gmail.com>
2025-04-17 00:44:53 +0600

    feat: add otp and otpCreateAt fields to users model for enhanced security

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -5,6 +5,8 @@ const usersSchema = new mongoose.Schema(
     phone: { type: String, required: true, unique: true },
     email: { type: String, unique: true, sparse: true, default: undefined },
     password: { type: String },
+    otp: {type: String},
+    otpCreateAt: {type: new Date},
     isDelete: { type: Boolean, default: false},
     role: { type: String, enum: ["user", "admin"], default: "user" },
     name: { type: String },
