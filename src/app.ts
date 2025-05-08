e7bc870aee8c6507472e52541fc99532376d1ab0
safi miazi <mohibullamiazi@gmail.com>
2025-05-08 16:29:16 +0600

    chore: Add a blank line for improved readability in app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
+
 app.use(cors({
   origin: "http://localhost:3000", // frontend URL
   credentials: true               // jodi cookie/token pathaos
