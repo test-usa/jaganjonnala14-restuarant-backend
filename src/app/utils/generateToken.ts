2f4a6ef91de2e0f251a5ed577fcf3b67ab86d043
safimiazi <mohibullamiazi@gmail.com>
2025-05-11 09:28:09 +0600

    fix: Add TypeScript ignore comment for jwt.sign function in generateToken utility

--- src/app/utils/generateToken.ts
+++ src/app/utils/generateToken.ts
@@ -1,5 +1,7 @@
+
 import jwt from "jsonwebtoken";
 const generateToken = (payload: object,secret: string, expiresIn : string | number) : string => {
+ //@ts-ignore
   return jwt.sign(payload, secret, {expiresIn})
 }
 export default generateToken;
