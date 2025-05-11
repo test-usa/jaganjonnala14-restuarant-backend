03e21bb3e3d48512a656d8fa3a30ce4e01bf6596
safimiazi <mohibullamiazi@gmail.com>
2025-05-11 08:22:21 +0600

    chore: Update DATABASE_URL in .env and remove unused route imports in index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,9 @@
 import { Router } from "express";
 import { authRoutes } from "../modules/auth/auth.routes";
 import { usersRoutes } from "../modules/users/users.routes";
-import { productsRoutes } from "../modules/products/products.routes";
-import { categoriesRoutes } from "../modules/categories/categories.routes";
-import { ordersRoutes } from "../modules/orders/orders.routes";
+
 import { adminRoutes } from "../modules/admin/admin.routes";
-import { customerRoutes } from "../modules/customer/customer.routes";
-import { brandRoutes } from "../modules/brand/brand.routes";
-import { attributeOptionRoutes } from "../modules/attributeOption/attributeOption.routes";
-import { attributeRoutes } from "../modules/attribute/attribute.routes";
-import { employeeRoutes } from "../modules/employee/employee.routes";
+
 
 const router = Router();
 
@@ -26,38 +20,7 @@ const moduleRoutes = [
         path: "/admin",
         route: adminRoutes
     },
-    {
-        path: "/customers",
-        route: customerRoutes
-    },
-    {
-        path: "/employee",
-        route: employeeRoutes
-    },
-    {
-        path: "/brand",
-        route: brandRoutes
-    },
-    {
-        path: "/atribute",
-        route: attributeRoutes
-    },
-    {
-        path: "/atribute_option",
-        route: attributeOptionRoutes
-    },
-    {
-        path: "/product",
-        route: productsRoutes
-    },
-    {
-        path: "/category",
-        route: categoriesRoutes
-    },
-    {
-        path: "/orders",
-        route: ordersRoutes
-    },
+ 
  
 ]
 
